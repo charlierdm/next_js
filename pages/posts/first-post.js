92fedd11bfc3a46a3b75fba343376b8b2644df95
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2023-07-07 10:39:51 +0100

    add next Head component to first-post

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,11 @@
+import Head from "next/head"
 import Link from "next/link"
 
 export default () =>
   <>
+    <Head>
+      <title>First Post</title>
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">Back to home</Link>
